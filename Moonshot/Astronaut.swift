0b403d635acc9f9a32c52e94b5ffd552438114d2
Ramin <rmnznd@gmail.com>
2025-08-06 00:13:32 -0700

    create 2 swift files 'Astronaut and Bundle-Decodable to handle imported files

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronauts.swift
+//  Moonshot
+//
+//  Created by Ramin Zand on 8/5/25.
+//
+
+import Foundation
+
+class Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let astronauts = Bundle.main.decode("astronauts.json")
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
